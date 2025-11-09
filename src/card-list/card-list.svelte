5839df92f2e860b5c828afe86b48caab5e093d56
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2025-11-09 15:54:26 +0000

    flexbox and grid section

--- src/card-list/card-list.svelte
+++ src/card-list/card-list.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
   import Card from '../card/card.svelte';
+  import PrimaryButton from '../button/primary-button.svelte';
 </script>
 
-<div class="space-y-4">
+<div class="flex items-center gap-2">
+  <div>
+    <input type="checkbox" />
+  </div>
+  <div>
+    <h2>Unsubscribe</h2>
+    <PrimaryButton />
+  </div>
+</div>
+
+<div class="flex flex-col gap-4 md:flex-row">
   <Card title="Card Title" description="This is a description of the card." />
   <Card title="Another Card" description="This is another description of a different card." />
   <Card title="Third Card" description="This is a description of the third card." />
--- src/navigation/navigation.svelte
+++ src/navigation/navigation.svelte
@@ -1,15 +1,21 @@
 {#snippet link(title)}
-  <li>
-    <a href="#">{title}</a>
+  <li class="group border-b-4 hover:bg-red-200">
+    <a href="#" class="p-2">{title}</a>
   </li>
 {/snippet}
 
-<nav>
-  <a href="/">Unicorn Corp</a>
-  <ul>
+<nav class="flex justify-between">
+  <a class="text-xl font-extrabold" href="/">Unicorn Corp</a>
+  <ul class="flex gap-4">
     {@render link('Home')}
     {@render link('About')}
     {@render link('Services')}
     {@render link('Contact')}
   </ul>
 </nav>
+
+<div class="flex gap-2">
+  <div class="bg-purple-500">1</div>
+  <div class="bg-purple-500">2</div>
+  <div class="bg-purple-500">3</div>
+</div>
